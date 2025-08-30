fa0705fd505488cf4a6ad93efd3a05807808568b
Salman Farsi <sfjadi2010@live.com>
2025-08-30 10:18:12 -0400

    Add user tracking to UserHub and update UI components
    - Introduced `TotalUsers` property in `UserHub.cs` to track connected users.
    - Updated `OnConnectedAsync` and `OnDisconnectedAsync` methods to manage user count and notify clients.
    - Added UI elements in `Index.cshtml` to display total users.
    - Modified script references in `_Layout.cshtml` for proper loading.
    - Enhanced `userCount.js` to handle user count updates and establish SignalR connection.

--- SignalRPractice/Hubs/UserHub.cs
+++ SignalRPractice/Hubs/UserHub.cs
@@ -6,10 +6,25 @@ public class UserHub : Hub
 {
     public static int TotalViews { get; set; } = 0;
 
+    public static int TotalUsers { get; set; } = 0;
+
+    public override Task OnConnectedAsync()
+    {
+        TotalUsers++;
+        Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        TotalUsers--;
+        Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+        return base.OnDisconnectedAsync(exception);
+    }
+
     public async Task NewWindowLoaded()
     {
         TotalViews++;
-
-        await Clients.All.SendAsync("UpdateTotalViews", TotalViews);
+        await Clients.All.SendAsync("updateTotalViews", TotalViews);
     }
 }
--- SignalRPractice/wwwroot/js/userCount.js
+++ SignalRPractice/wwwroot/js/userCount.js
@@ -2,20 +2,26 @@
 // create connection
 var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
 // connect to methods that hub invokes aka receive notifications from hub
-connectionCount.on("updateTotalViews", (value) => {
+connectionUserCount.on("updateTotalViews", (value) => {
     var newCountSpan = document.getElementById("totalViewsCounter");
     newCountSpan.innerText = value.toString();
 });
 
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
 // invoke hub methods aka send notificatin to hub
 const newWindowLoadedOnClient = () => {
     connectionUserCount.send("NewWindowLoaded");
+
 };
 
 // start connection
-const fulfilled = () => {
+const fulfilled = async () => {
     // do something on start
     console.log("Connection is successful to SignalR Hub.");
+    await newWindowLoadedOnClient();
 }
 
 const rejected = () => {
